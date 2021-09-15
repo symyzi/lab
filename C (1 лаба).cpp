810247ad6c3d278d9f44cb34c021d9f91ebd2d90
symyzi <89811910+symyzi@users.noreply.github.com>
2021-09-15 08:22:08 +0300

    Add files via upload

---
+++ C (1 лаба).cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>;
+
+int main() {
+	int var;
+	char sym;
+	scanf("%d\n", &var);
+	scanf("%c", &sym);
+	printf("%d\n", var);
+	printf("%c", sym);
+
+
+	return 0;
+}
